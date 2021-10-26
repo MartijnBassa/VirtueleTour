0e907699122dca4082cdced44486a8d99c168169
Martijn <Martijnbassa2009@hotmail.com>
2021-10-26 11:11:51 +0200

    Fixed switching between 360 video clips

--- VR Housing Brochure/Assets/Scripts/Next360Video.cs
+++ VR Housing Brochure/Assets/Scripts/Next360Video.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Next360Video : MonoBehaviour
 {
     [SerializeField] private Material[] _skyBoxes;
-
     [SerializeField] private int _videoIndex = 0;
 
+    [SerializeField] private GameObject[] _equirectangularVideoClips;
+
+    // Set skybox to 360 video texture, activate object to play video
     public void NextVideo()
     {
         _videoIndex++;
@@ -17,6 +19,16 @@ public class Next360Video : MonoBehaviour
         }
 
         RenderSettings.skybox = _skyBoxes[_videoIndex];
+
+        // Deactivate every 360 video clip object
+        for(int i = 0; i < _equirectangularVideoClips.Length; i++)
+        {
+            _equirectangularVideoClips[i].SetActive(false);
+        }
+
+        // Activate 360 video clip object with same index as skybox
+        _equirectangularVideoClips[_videoIndex].SetActive(true);
+
 
     }
 }
