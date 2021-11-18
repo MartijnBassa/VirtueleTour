9343629845e47aed17f7295dc10d03b977262495
Martijn <Martijnbassa2009@hotmail.com>
2021-11-18 14:56:12 +0100

    Test fade animation

--- VR Housing Brochure/Assets/Scripts/Next360Photo.cs
+++ VR Housing Brochure/Assets/Scripts/Next360Photo.cs
@@ -1,28 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Next360Photo : MonoBehaviour
 {
 
-    [SerializeField] private Material _equirectangularPhotoMaterial;
+    //[SerializeField] private Material _equirectangularPhotoMaterial;
     [SerializeField] private GameObject _equirectangularPhotoSphere;
-    [SerializeField] private Texture[] _equirectangularPhotoTexture;
+    [SerializeField] private Texture[] _equirectangularPhotoTextures;
 
     [SerializeField] private int _photoIndex = 0;
 
+    [SerializeField] private GameObject _fadeUI;
+
+    // Change spheres texture to new texture
     public void NextPhoto()
     {
 
         _photoIndex++;     
-        if(_photoIndex > _equirectangularPhotoTexture.Length - 1)
+        if(_photoIndex > _equirectangularPhotoTextures.Length - 1)
         {
             _photoIndex = 0;
         }
-
-        _equirectangularPhotoSphere.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", _equirectangularPhotoTexture[_photoIndex]);
+        _fadeUI.GetComponent<Animation>().Play("FadeAnimation");
+        _equirectangularPhotoSphere.GetComponent<MeshRenderer>().material.SetTexture("_MainTex", _equirectangularPhotoTextures[_photoIndex]);
+        _fadeUI.GetComponent<Animation>().Play("FadeOutAnimation");
 
     }
+
 
 
 }
