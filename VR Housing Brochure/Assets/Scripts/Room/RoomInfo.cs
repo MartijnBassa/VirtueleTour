dfacb31c2247be56d08f1da97fc3fb7275489bef
MartijnBassa <49745039+MartijnBassa@users.noreply.github.com>
2021-12-07 16:34:34 +0100

    Add audio begin objects

--- VR Housing Brochure/Assets/Scripts/Room/RoomInfo.cs
+++ VR Housing Brochure/Assets/Scripts/Room/RoomInfo.cs
@@ -8,9 +8,11 @@ public class RoomInfo
     [SerializeField] private RoomType _roomType;
     [SerializeField] private Room _room;
     [SerializeField] private Material _material;
+    [SerializeField] private AudioClip _audioClip;
 
     public RoomType RoomType => _roomType;
     public Room Room => _room;
     public Material Material => _material;
+    public AudioClip AudioClip => _audioClip;
 
 }
