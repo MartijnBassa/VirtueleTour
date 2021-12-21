03ed9c5292ba1bbe78742b5357b99620f2a8e331
MartijnBassa <49745039+MartijnBassa@users.noreply.github.com>
2021-12-21 13:41:21 +0100

    Add audio timer

--- VR Housing Brochure/Assets/Scripts/Room/RoomManager.cs
+++ VR Housing Brochure/Assets/Scripts/Room/RoomManager.cs
@@ -20,6 +20,8 @@ public class RoomManager : Singleton<RoomManager>
     private RoomInfo _currentActiveRoomInfo;
 
     private int _numberOfImages;
+    public float _welcomeTimer;
+    private float _timeToWelcome = 60f;
 
     public int NumberOfImages => _numberOfImages;
 
@@ -29,6 +31,13 @@ public class RoomManager : Singleton<RoomManager>
         SwitchRoom(_startRoomType);
 
         _numberOfImages = _currentActiveRoomInfo.SwitchRoomTextures.Length;
+
+    }
+
+    private void Update()
+    {
+        _welcomeTimer += Time.deltaTime;
+
     }
 
     public void SwitchRoom(RoomType roomType)
@@ -43,36 +52,10 @@ public class RoomManager : Singleton<RoomManager>
                     _currentActiveRoomInfo.Room.gameObject.SetActive(false);
                 }
 
-                // check if next room has skybox info, set skybox to 360 video
-                if(roomInfo.SkyboxMaterial != null)
-                {
-                    RenderSettings.skybox = roomInfo.SkyboxMaterial;
-                }
-                else
-                {
-                    RenderSettings.skybox = null;
-                }
+                SetSkybox(roomInfo);
+                AudioBackground(roomInfo);
+                AudioWelcome(roomInfo);
 
-                // check if next room has background audio info, set audiosource clip to background sound
-                if(roomInfo.AudioBackground != null)
-                {
-                    roomInfo.Room.gameObject.GetComponentInChildren<AudioSource>().clip = roomInfo.AudioBackground;
-                }
-
-                // check if next room has a person speaking, set clip to first welcome if first time entering else second welcome
-                if(roomInfo.SpeakerAudioSource != null)
-                {
-                    if (roomInfo.WelcomeCounter <= 0)
-                    {
-                        roomInfo.SpeakerAudioSource.clip = roomInfo.SpeakerClips[roomInfo.WelcomeCounter];
-                        CountWelcomes(roomInfo);
-                    }
-                    else
-                    {
-                        roomInfo.SpeakerAudioSource.clip = roomInfo.SpeakerClips[roomInfo.WelcomeCounter];
-                    }
-
-                }
 
                 // activate next room
                 roomInfo.Room.gameObject.SetActive(true);
@@ -102,13 +85,59 @@ public class RoomManager : Singleton<RoomManager>
         }
     }
 
+    private void AudioWelcome(RoomInfo roomInfo)
+    {
+        // check if next room has a person speaking, set clip to first welcome if first time entering else second welcome
+        if (roomInfo.SpeakerAudioSource != null)
+        {
+            if (roomInfo.WelcomeCounter == 0)
+            {
+                roomInfo.SpeakerAudioSource.clip = roomInfo.SpeakerClips[roomInfo.WelcomeCounter];
+                CountWelcomes(roomInfo);
+            }
+            else if(roomInfo.WelcomeCounter == 1)
+            {
+                roomInfo.SpeakerAudioSource.clip = roomInfo.SpeakerClips[roomInfo.WelcomeCounter];
+                CountWelcomes(roomInfo);
+
+            }
+            else
+            {
+                roomInfo.SpeakerAudioSource.clip = null;
+
+                if (_welcomeTimer >= _timeToWelcome)
+                {
+                    roomInfo.SpeakerAudioSource.clip = roomInfo.SpeakerClips[1];
+                    _welcomeTimer = 0f;
+                }
+            }
+        }
+    }
+
+    private void AudioBackground(RoomInfo roomInfo)
+    {
+        // check if next room has background audio info, set audiosource clip to background sound
+        if (roomInfo.AudioBackground != null)
+        {
+            roomInfo.Room.gameObject.GetComponentInChildren<AudioSource>().clip = roomInfo.AudioBackground;
+        }
+    }
+
+    private void SetSkybox(RoomInfo roomInfo)
+    {
+        // check if next room has skybox info, set skybox to 360 video
+        if (roomInfo.SkyboxMaterial != null)
+        {
+            RenderSettings.skybox = roomInfo.SkyboxMaterial;
+        }
+        else
+        {
+            RenderSettings.skybox = null;
+        }
+    }
+
     private void CountWelcomes(RoomInfo roomInfo)
     {
         roomInfo.WelcomeCounter++;
-
-        if(roomInfo.WelcomeCounter >= 1)
-        {
-            roomInfo.WelcomeCounter = 1;
-        }
-    }    
+    }
 }
